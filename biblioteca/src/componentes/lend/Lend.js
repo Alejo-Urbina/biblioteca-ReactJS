93d95032cdeb7d47eb4e5f31f83f9371a994846f
Alejandro <alejoub1998@gmail.com>
2023-08-27 22:33:46 -0500

    Se agrega componente de Lend

---
+++ biblioteca/src/componentes/lend/Lend.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import './Lend.css'; // Importa los estilos CSS
+
+const Lend = () => {
+  const [bookCount, setBookCount] = useState(0);
+
+  const incrementBookCount = () => {
+    setBookCount(bookCount + 1);
+  };
+
+  const calculateTotalCost = () => {
+    const bookPrice = 500;
+    return bookCount * bookPrice;
+  };
+
+  return (
+    <div className="library-container">
+      <h1 className="library-title">Biblioteca</h1>
+      <p className="book-count">Libros solicitados prestamo: {bookCount}</p>
+      <p className="total-cost">Costo total: ${calculateTotalCost()}</p>
+      <button className="request-button" onClick={incrementBookCount}>
+        Solicitar Libro
+      </button>
+    </div>
+  );
+};
+
+export default Lend;
