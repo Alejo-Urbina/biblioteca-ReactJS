c733406a1729fd6c50be4065683be7777b443d9d
Alejandro <alejoub1998@gmail.com>
2023-08-27 20:53:46 -0500

    Se agrega nuevo componente donde por el momento va incluido solo el Body.js

---
+++ biblioteca/src/componentes/body/Body.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Body.css'; // Importa los estilos si los tienes
+
+function Body() {
+  return (
+    <div className="library-body">
+      <h2>Explora Nuestra Colección de Libros</h2>
+    </div>
+  );
+}
+
+export default Body;
