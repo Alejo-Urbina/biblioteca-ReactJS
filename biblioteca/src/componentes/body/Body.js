ca5942fa5d4d610f36ca9a855347c6b9010c360d
Alejandro <alejoub1998@gmail.com>
2023-08-27 22:33:15 -0500

    Se agrega componente Lend a Body

--- biblioteca/src/componentes/body/Body.js
+++ biblioteca/src/componentes/body/Body.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './Body.css'; // Importa los estilos si los tienes
 import List from '../list/List';
+import Lend from '../lend/Lend';
 
 function Body() {
   return (
     <div className="library-body">
       <h2>Explora Nuestra Colección de Libros</h2>
       <List />
+      <Lend />
     </div>
   );
 }
