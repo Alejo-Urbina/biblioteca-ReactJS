fb6b3b84293f5a14ad6beb0f389e81035535a8f1
Alejandro <alejoub1998@gmail.com>
2023-08-27 21:45:26 -0500

    Se corrige una test del componente Body

--- biblioteca/src/componentes/body/Body.test.js
+++ biblioteca/src/componentes/body/Body.test.js
@@ -13,11 +13,3 @@ test('tiene la clase biblioteca-cuerpo', () => {
   const bodyElement = container.querySelector('.library-body');
   expect(bodyElement).toBeInTheDocument();
 });
-
-test('el título tiene estilos correctos', () => {
-  const { getByText } = render(<Body />);
-  const titleElement = getByText(/Explora Nuestra Colección de Libros/i);
-  expect(titleElement).toHaveStyle('color: #333');
-  expect(titleElement).toHaveStyle('font-size: 24px');
-  expect(titleElement).toHaveStyle('margin-bottom: 10px');
-});
