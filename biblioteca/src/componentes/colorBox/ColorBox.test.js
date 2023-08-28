2d16f75f03f0fb9b437b94e590776a2dd1791a00
Alejandro <alejoub1998@gmail.com>
2023-08-27 22:56:43 -0500

    Se le agregan test al comboBox

---
+++ biblioteca/src/componentes/colorBox/ColorBox.test.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import ColorBox from './ColorBox';
+
+describe('ColorBox', () => {
+  it('renders without crashing', () => {
+    render(<ColorBox />);
+  });
+
+  it('changes color on click', () => {
+    const { getByTestId } = render(<ColorBox />);
+    const colorBox = getByTestId('color-box');
+
+    const initialColor = colorBox.style.backgroundColor;
+    fireEvent.click(colorBox);
+    const newColor = colorBox.style.backgroundColor;
+
+    expect(newColor).not.toBe(initialColor);
+  });
+
+  it('changes color on button click', () => {
+    const { getByTestId, getByText } = render(<ColorBox />);
+    const colorBox = getByTestId('color-box');
+    const changeColorButton = getByText('Cambiar Color');
+
+    const initialColor = colorBox.style.backgroundColor;
+    fireEvent.click(changeColorButton);
+    const newColor = colorBox.style.backgroundColor;
+
+    expect(newColor).not.toBe(initialColor);
+  });
+});
