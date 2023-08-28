9fef384c908af08c11ccf8cbfcad31ab4e6f752f
Alejandro <alejoub1998@gmail.com>
2023-08-27 21:47:15 -0500

    Se agrega el componente List en el componente Body

--- biblioteca/src/componentes/body/Body.js
+++ biblioteca/src/componentes/body/Body.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Body.css'; // Importa los estilos si los tienes
+import List from '../list/List';
 
 function Body() {
   return (
     <div className="library-body">
       <h2>Explora Nuestra Colección de Libros</h2>
+      <List />
     </div>
   );
 }
